37a7e28b7ed240945abe7ddf7927739cef152054
Alaa Zamel <alaa.zamel23@gmail.com>
2024-05-05 22:36:31 +0300

    feat: create a training script

--- text_processors/antique_text_processor.py
+++ text_processors/antique_text_processor.py
@@ -1,11 +1,13 @@
 from typing import List
 from overrides import overrides
+
 from text_processors.base_text_processor import BaseTextProcessor
+
 
 class AntiqueTextProcessor(BaseTextProcessor):
 
     @overrides
     def process(self, text) -> List[str]:
-        processed_text = super().process(text)
-
+        processed_text = self.remove_stopwords(text)
+        processed_text = self.lemmatize(processed_text)  # Add lemmatization
         return processed_text
--- text_processors/base_text_processor.py
+++ text_processors/base_text_processor.py
@@ -4,24 +4,21 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 import string
 
+
 class BaseTextProcessor:
 
     def process(self, text) -> List[str]:
-        text_lower = self.convert_to_lowercase(text)
-        processed_text = self.remove_stopwords(text_lower)
-        processed_text = self.lemmatize(processed_text)  # Add lemmatization
-        return processed_text
+        pass
 
-    def convert_to_lowercase(self, text: str) -> str:
-        return text.lower()
-
-    def remove_stopwords(self, text: str) -> List[str]:
+    @staticmethod
+    def remove_stopwords(text: str) -> List[str]:
         stop_words = set(stopwords.words('english'))
         tokens = word_tokenize(text)
         filtered_text = [word for word in tokens if word not in stop_words and word not in string.punctuation]
         return filtered_text
 
-    def lemmatize(self, tokens: List[str]) -> List[str]:
+    @staticmethod
+    def lemmatize(tokens: List[str]) -> List[str]:
         lemmatizer = WordNetLemmatizer()
         lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
         return lemmatized_tokens
