4494eb1cb227fd61abbcf74304c94f599890411d
MoFares7 <faresdabbas1@gmail.com>
2024-05-05 22:38:39 +0300

    apply another way on lemmatize

--- text_processors/base_text_processor.py
+++ text_processors/base_text_processor.py
@@ -2,6 +2,8 @@ from typing import List
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+from nltk.corpus import wordnet
 import string
 
 class BaseTextProcessor:
@@ -9,7 +11,7 @@ class BaseTextProcessor:
     def process(self, text) -> List[str]:
         text_lower = self.convert_to_lowercase(text)
         processed_text = self.remove_stopwords(text_lower)
-        processed_text = self.lemmatize(processed_text)  # Add lemmatization
+        processed_text = self.lemmatize(processed_text)
         return processed_text
 
     def convert_to_lowercase(self, text: str) -> str:
@@ -23,5 +25,14 @@ class BaseTextProcessor:
 
     def lemmatize(self, tokens: List[str]) -> List[str]:
         lemmatizer = WordNetLemmatizer()
-        lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+        pos_tags = pos_tag(tokens)
+        lemmatized_tokens = [lemmatizer.lemmatize(token, pos=self.get_wordnet_pos(tag)) for token, tag in pos_tags]
         return lemmatized_tokens
+
+    def get_wordnet_pos(self, tag_parameter):
+        tag = tag_parameter[0].upper()
+        tag_dict = {"J": wordnet.ADJ,
+                    "N": wordnet.NOUN,
+                    "V": wordnet.VERB,
+                    "R": wordnet.ADV}
+        return tag_dict.get(tag, wordnet.NOUN)
