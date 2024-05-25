954921ae11d71357d0acc9eb3c367b33d37f1855
Alaa Zamel <alaa.zamel23@gmail.com>
2024-05-25 08:47:06 +0300

    refactor: make major changes in embedding models aiming to improve accuracy.

--- models/embedding/base_embedding_model.py
+++ models/embedding/base_embedding_model.py
@@ -1,11 +1,8 @@
-import decimal
 import os
-from typing import Optional
 
 import numpy as np
 from chromadb.api.models.Collection import Collection
-from gensim.models import Doc2Vec, Word2Vec
-from gensim.models.doc2vec import TaggedDocument
+from gensim.models import Word2Vec
 
 from common.constants import Locations
 from common.file_utilities import FileUtilities
@@ -93,7 +90,13 @@ class BaseEmbeddingModel:
 
             for doc in batch:
                 # Collect word vectors for the current document
-                doc_vectors = [self.model.wv[word] for word in doc['processed_doc'] if word in self.model.wv]
+                doc_vectors = []
+                for word in doc['processed_doc']:
+                    if word in self.model.wv:
+                        try:
+                            doc_vectors.append(self.model.wv[word])
+                        except KeyError:
+                            doc_vectors.append(np.random.rand(self.vector_size))
 
                 if doc_vectors:
                     # Calculate the mean vector if there are valid word vectors
--- text_processors/base_text_processor.py
+++ text_processors/base_text_processor.py
@@ -8,6 +8,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag, PorterStemmer
 from nltk.corpus import wordnet
 import string
+import logging
 
 from spellchecker import SpellChecker
 
@@ -27,144 +28,129 @@ def get_wordnet_pos(tag_parameter):
 
 
 class BaseTextProcessor:
-
-    def __init__(self):
+    def __init__(self) -> None:
         self.stop_words = set(stopwords.words('english'))
+        self.spell_checker = SpellChecker(distance=4)
         self.lemmatizer = WordNetLemmatizer()
-        self.pos_tagger = pos_tag
-        self.spell_checker = SpellChecker()
-        # ---------------------------------
+        self.tokenizer = word_tokenize
         self.stemmer = PorterStemmer()
+        self.pos_tagger = pos_tag
+        self.coco = coco
+
+        # disable logger
+        self.coco.logging.disable()
 
     def process(self, text) -> List[str]:
         pass
 
+    def process_query(self, query: str) -> List[str]:
+        pass
+
+    def _word_tokenizer(self, text: str) -> List[str]:
+        tokens = self.tokenizer(text)
+        return tokens
+
     @staticmethod
-    def _word_tokenizer(text: str) -> List[str]:
-        return word_tokenize(text)
+    def _lowercase_tokens(tokens: List[str]) -> List[str]:
+        return [str(np.char.lower(token)) for token in tokens]
 
-    def _remove_stopwords(self, tokens: List[str]) -> List[str]:
-        filtered_text = [word for word in tokens if word not in self.stop_words]
-        return filtered_text
+    def _filter_stop_words(self, tokens: List[str]) -> List[str]:
+        return [token for token in tokens if token not in self.stop_words and len(token) > 1]
 
-    def _lemmatize(self, tokens: List[str]) -> List[str]:
+    @staticmethod
+    def _remove_registered_markers(tokens: List[str]) -> List[str]:
+        return [re.sub(r'\u00AE', '', token) for token in tokens]
+
+    @staticmethod
+    def _strip_punctuation(tokens: List[str]) -> List[str]:
+        return [token.translate(str.maketrans('', '', string.punctuation)) for token in tokens if token is not None]
+
+    @staticmethod
+    def _eliminate_whitespaces(tokens: List[str]) -> List[str]:
+        return [token.replace('_', ' ') for token in tokens]
+
+    @staticmethod
+    def _remove_apostrophes(tokens: List[str]) -> List[str]:
+        return [token.replace("'", " ") for token in tokens if token is not None]
+
+    def _apply_stemming(self, tokens: List[str]) -> List[str]:
+        return [self.stemmer.stem(token) for token in tokens]
+
+    @staticmethod
+    def _normalize_abbreviations(tokens: List[str]) -> List[str]:
+        resolved_terms = {
+            token: wordnet.synsets(token)[0].lemmas()[0].name()
+            for token in tokens if len(token) >= 2 and wordnet.synsets(token)
+        }
+
+        return [resolved_terms.get(token, token) for token in tokens]
+
+    def _lemmatize_tokens(self, tokens: List[str]) -> List[str]:
         lemmatizer = self.lemmatizer
         pos_tags = self.pos_tagger(tokens)
         lemmatized_tokens = [lemmatizer.lemmatize(token, pos=get_wordnet_pos(tag)) for token, tag in pos_tags]
         return lemmatized_tokens
 
-    @staticmethod
-    def _remove_punctuations(tokens: List[str]) -> List[str]:
-        return [token.translate(str.maketrans('', '', string.punctuation)) for token in tokens if token is not None]
-
-    @staticmethod
-    def _remove_whitespaces(tokens: List[str]) -> List[str]:
-        return [token for token in tokens if token.strip()]
-
     def _spell_check(self, tokens: List[str]) -> List[str]:
-        return [self.spell_checker.correction(token) for token in tokens]
-
-    # ---------------------------------------------------------------------------
-    @staticmethod
-    def remove_markers(tokens: List[str]) -> List[str]:
-        new_tokens = []
-        for token in tokens:
-            new_tokens.append(re.sub(r'\u00AE', '', token))
-        return new_tokens
+        return [self.spell_checker.correction(word) for word in tokens if word is not None]
 
     @staticmethod
-    def replace_under_score_with_space(tokens: List[str]) -> List[str]:
-        new_tokens = []
+    def get_tokens_as_string(tokens: List[str]) -> str:
+        return ' '.join(tokens)
+
+    def _normalize_country_name(self, tokens: List[str]) -> List[str]:
+        normalized_text = []
         for token in tokens:
-            new_tokens.append(re.sub(r'_', ' ', token))
-        return new_tokens
+            standard_name = self.coco.convert(names=token, to='ISO3', not_found=None)
+            if standard_name is not None:
+                normalized_text.append(standard_name)
+            else:
+                normalized_text.append(token)
+        return normalized_text
 
     @staticmethod
-    def remove_apostrophe(tokens: List[str]) -> List[str]:
-        new_tokens = []
+    def _normalize_months(tokens: List[str]) -> List[str]:
+        normalized_tokens = []
         for token in tokens:
-            new_tokens.append(str(np.char.replace(token, "'", " ")))
-        return new_tokens
+            if token.capitalize() in calendar.month_abbr:
+                full_month_name = calendar.month_name[list(calendar.month_abbr).index(token.capitalize())]
+                normalized_tokens.append(full_month_name.lower())
+            else:
+                normalized_tokens.append(token)
+        return normalized_tokens
 
     @staticmethod
-    def normalize_abbreviations(tokens: List[str]) -> List[str]:
-        resolved_terms = {}
+    def _normalize_days(tokens: List[str]) -> List[str]:
+        # Regular expression pattern to match day of the month
+        day_pattern = r'\b(\d{1,2})(?:st|nd|rd|th)?\b(?![/\-.\d])'
+        # Normalize each token in the list
+        normalized_tokens = []
         for token in tokens:
+            match = re.match(day_pattern, token)
+            if match:
+                normalized_tokens.append(str(int(match.group(1))))  # Convert to integer to remove leading zeros
+            else:
+                normalized_tokens.append(token)
+        return normalized_tokens
 
-            if len(token) >= 2:
-                synsets = wordnet.synsets(token)
-                if synsets:
-                    resolved_term = synsets[0].lemmas()[0].name()
-                    resolved_terms[token] = resolved_term
-
-        for abbreviation, resolved_term in resolved_terms.items():
-            for i in range(len(tokens)):
-                if tokens[i] == abbreviation:
-                    tokens[i] = resolved_term
-                    break
-
-        return tokens
-
-    def stemming(self, tokens: List[str]) -> List[str]:
-        new_tokens = []
+    @staticmethod
+    def _normalize_dates(tokens: List[str]) -> List[str]:
+        normalized_tokens = []
         for token in tokens:
-            new_tokens.append(self.stemmer.stem(token))
-        return new_tokens
-
+            parsed_date = dateparser.parse(token, settings={'STRICT_PARSING': True})
+            if parsed_date is not None:
+                # Extract day, month, and year components from the parsed date
+                day = str(parsed_date.day)
+                month = calendar.month_name[parsed_date.month]
+                year = str(parsed_date.year)
+                # Append the extracted components as names
+                normalized_tokens.extend([day, month, year])
+            else:
+                normalized_tokens.append(token)
+        return normalized_tokens
 
 # ---------------------------------------------------------------------------
 
-def _normalize_months(tokens: List[str]) -> List[str]:
-    normalized_tokens = []
-    for token in tokens:
-        if token.capitalize() in calendar.month_abbr:
-            full_month_name = calendar.month_name[list(calendar.month_abbr).index(token.capitalize())]
-            normalized_tokens.append(full_month_name.lower())
-        else:
-            normalized_tokens.append(token)
-    return normalized_tokens
-
-
-def _normalize_days(tokens: List[str]) -> List[str]:
-    # Regular expression pattern to match day of the month
-    day_pattern = r'\b(\d{1,2})(?:st|nd|rd|th)?\b(?![/\-.\d])'
-    # Normalize each token in the list
-    normalized_tokens = []
-    for token in tokens:
-        match = re.match(day_pattern, token)
-        if match:
-            normalized_tokens.append(str(int(match.group(1))))  # Convert to integer to remove leading zeros
-        else:
-            normalized_tokens.append(token)
-    return normalized_tokens
-
-
-def _normalize_dates(tokens: List[str]) -> List[str]:
-    normalized_tokens = []
-    for token in tokens:
-        parsed_date = dateparser.parse(token, settings={'STRICT_PARSING': True})
-        if parsed_date is not None:
-            # Extract day, month, and year components from the parsed date
-            day = str(parsed_date.day)
-            month = calendar.month_name[parsed_date.month]
-            year = str(parsed_date.year)
-            # Append the extracted components as names
-            normalized_tokens.extend([day, month, year])
-        else:
-            normalized_tokens.append(token)
-    return normalized_tokens
-
-
-def _normalize_country_name(tokens: List[str]) -> List[str]:
-    normalized_text = []
-    for token in tokens:
-        standard_name = coco.convert(names=token, to='name_short', not_found=None, )
-        # print(standard_name)
-        if standard_name is not None:
-            normalized_text.append(standard_name)
-        else:
-            normalized_text.append(token)
-    return normalized_text
 
 # tokens = _normalize_months(tokens)
 #      tokens = _normalize_days(tokens)
